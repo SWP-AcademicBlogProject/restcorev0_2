4ec3a5048e47f9dbf6ad85446f2a6d1cb3f67a1f
minhtruong123A <truonghaiminh2112@gmail.com>
2023-10-18 21:18:58 +0700

    changing model api to mvc 9:18pm 18/10/23

---
+++ src/main/java/test/fptblog/controller/loginController.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.fptblog.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import test.fptblog.model.loginForm;
+import test.fptblog.model.logoutForm;
+import test.fptblog.model.userModel;
+import test.fptblog.repositories.userRepository;
+
+/**
+ *
+ * @author Dell
+ */
+@Controller
+public class loginController {
+   
+
+        @RequestMapping(value="/Background", method=RequestMethod.GET)
+        public String Background(){
+            return "index";
+        }
+    
+        @RequestMapping(value="/login", method=RequestMethod.GET)
+        public String getloginObj(){
+            return "loginnnn";
+        }
+        
+        @RequestMapping(value="/homepage", method=RequestMethod.GET)
+        public String accesshomepage(){
+            return "viewforum";
+        }
+        @RequestMapping(value="/profile_user", method=RequestMethod.GET)
+    public String profileuser(){
+        return "profile";
+    }
+    
+    
+        @RequestMapping(value="/login", method=RequestMethod.POST)
+        public String login(@ModelAttribute(name="loginForm") loginForm loginForm, Model model){
+        String  username= loginForm.getUsername();
+        String password= loginForm.getPassword();
+        System.out.println("UI:" + loginForm);       
+//        if(username.matches("^[a-z0-9](\\.?[a-z0-9]){5,}@fpt\\.edu\\.vn$") || username.equals("minh")){
+            return "viewforum";
+//        }
+//        model.addAttribute("invalidCredentials",true);
+//        return "loginnnn";
+//        ModelAndView modelAndView=new ModelAndView();
+//        modelAndView.setViewName("viewforum");
+//        modelAndView.addObject("loginO",loginObj);
+//        return modelAndView;
+    }
+        
+        
+    @RequestMapping(value="/loginadmin", method=RequestMethod.GET)
+    public String loginPage(){
+        return "auth-login";
+    }
+
+    @RequestMapping(value="/system", method=RequestMethod.GET)
+    public String system(){
+        return "landing_system";
+    }
+    
+    @RequestMapping(value="/profileAdmin", method=RequestMethod.GET)
+    public String profileAdmin(){
+        return "landing_profile";
+    }
+    
+    @RequestMapping("/home")
+    public String loginSubmit(){
+        return "landing_page";
+    }
+    
+           @RequestMapping(value="/loginadmin", method=RequestMethod.POST)
+        public String loginadmin(@ModelAttribute(name="loginForm") loginForm loginForm, Model model){
+        String  username= loginForm.getUsername();
+        String  password= loginForm.getPassword();
+        System.out.println("UI:" + loginForm);       
+//        if("admin".equals(username)){
+            return "landing_page";
+//        }
+//        model.addAttribute("invalidCredentials",true);
+//        return "auth-login";
+        
+//        ModelAndView modelAndView=new ModelAndView();
+//        modelAndView.setViewName("viewforum");
+//        modelAndView.addObject("loginO",loginObj);
+//        return modelAndView;
+    }
+        
+    @RequestMapping(value="/register", method=RequestMethod.GET)
+    public String RegisterPage(){
+        return "create";
+    }
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+        public String Register(@ModelAttribute(name="logoutForm") logoutForm logoutForm, Model model){
+        System.out.println("UI:" + logoutForm);       
+//        if(username.matches("^[a-z0-9](\\.?[a-z0-9]){5,}@fpt\\.edu\\.vn$") || username.equals("minh")){
+            return "loginnnn";
+}
+    @RequestMapping(value="/profileAdmin", method=RequestMethod.POST)
+        public String updateProfile(@ModelAttribute(name="logoutForm") logoutForm logoutForm, Model model){
+        System.out.println("UI:" + logoutForm);       
+//        if(username.matches("^[a-z0-9](\\.?[a-z0-9]){5,}@fpt\\.edu\\.vn$") || username.equals("minh")){
+            return "landing_profile";
+}
+        
+}
