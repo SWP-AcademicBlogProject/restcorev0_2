489754d4a2b468907478817d1e16aefd82bf9daa
yeuMyrrh <manhkhai2606@gmail.com>
2023-10-19 21:09:29 +0700

    change api call method to MVC traditional method

---
+++ src/main/java/test/fptblog/controller/loginController.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.fptblog.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import test.fptblog.model.userModel;
+import test.fptblog.repositories.userRepository;
+
+/**
+ *
+ * @author pc
+ */
+@Controller
+public class loginController {
+    
+     @Autowired
+    private userRepository repository;
+    
+    @RequestMapping(value = "/login")
+    public String login(HttpSession session, @RequestParam(value = "username") String username,@RequestParam(value = "password") String password ) {
+        
+        
+        
+        
+        List<userModel> founduser = repository.findByUserName(username);
+        if (founduser.isEmpty()) return "loginfailed";
+        if (password.matches(founduser.get(0).getPassword())) {
+            
+            System.out.println("user : " + founduser.get(0).getPassword() + " login successfully ");
+            session.setAttribute("user", founduser.get(0).getUserName());
+            return "viewforum";
+        } else {
+            System.out.println("login failed");
+            return "loginfailed";
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
--- src/main/java/test/fptblog/oldrestcontroller/userController.java
+++ src/main/java/test/fptblog/oldrestcontroller/userController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package test.fptblog.controller;
+package test.fptblog.oldrestcontroller;
 
 import java.util.List;
 import java.util.Optional;
