5e6ee907d0617a66ca1cfd1b45f45b980b923d13
yeuMyrrh <manhkhai2606@gmail.com>
2023-11-21 14:04:32 +0700

    [Back-end] fix bug category + subject

--- src/main/java/com/group8swp/fptblog/model/CategoryDTO.java
+++ src/main/java/com/group8swp/fptblog/model/CategoryDTO.java
@@ -8,6 +8,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -22,6 +24,7 @@ import javax.persistence.Table;
 public class CategoryDTO {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CategoryID")
     private int categoryId;
 
--- src/main/java/com/group8swp/fptblog/model/SubjectDTO.java
+++ src/main/java/com/group8swp/fptblog/model/SubjectDTO.java
@@ -9,6 +9,8 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -22,6 +24,7 @@ import javax.persistence.Table;
 @Table(name = "[dbo].[Subject]")
 public class SubjectDTO {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="SubjectID")
     private int subjectId;
     
